9cc88b687be480717fe13d8f7c282b449fc72e92
VladdyP <v.popudrebko@gmail.com>
2024-07-11 16:23:44 -0500

    Adding the second test

--- cypress/e2e/Connects_Staging.cy.js
+++ cypress/e2e/Connects_Staging.cy.js
@@ -1,5 +1,12 @@
 describe('Login Page test', () => {
-  it('passes', () => {
+  it('Test 01', () => {
+    cy.visit('/cupertino')
+    cy.get('[id="userName"]').type('Chris')
+    cy.get('.MuiButtonBase-root').should('have.text','Continue').click()
+    cy.get('[id="password"]').type('Welcome1!')
+    cy.get('[type="submit"]').click()
+  }),
+  it('Test 02', () => {
     cy.visit('/cupertino')
     cy.get('[id="userName"]').type('Chris')
     cy.get('.MuiButtonBase-root').should('have.text','Continue').click()
