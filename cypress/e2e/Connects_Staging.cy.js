77ac84285fbeb7413903876b0bcb65596c5e5745
VladdyP <v.popudrebko@gmail.com>
2024-07-11 15:57:58 -0500

    Test with Ksysha

--- cypress/e2e/Connects_Staging.cy.js
+++ cypress/e2e/Connects_Staging.cy.js
@@ -1,7 +1,7 @@
 describe('Login Page test', () => {
   it('passes', () => {
     cy.visit('/cupertino')
-    cy.get('[id="userName"]').type('vlad')
+    cy.get('[id="userName"]').type('Chris')
     cy.get('.MuiButtonBase-root').should('have.text','Continue').click()
     cy.get('[id="password"]').type('Welcome1!')
     cy.get('[type="submit"]').click()
