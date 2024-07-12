40f904dc2a8faf03c318ef341be60b6ff81bf41e
VladdyP <v.popudrebko@gmail.com>
2024-07-12 13:02:54 -0500

    Invalid password test

--- cypress/e2e/Connects_Staging.cy.js
+++ cypress/e2e/Connects_Staging.cy.js
@@ -19,5 +19,12 @@ describe('Login Page test', () => {
     cy.get('.MuiButtonBase-root').should('have.text','Continue').click()
     cy.get('[id="password"]').type('Welcome1!')
     cy.get('[type="submit"]').click()
+  }),
+  it('Test 04', () => {
+    cy.visit('/cupertino')
+    cy.get('[id="userName"]').type('Chris')
+    cy.get('.MuiButtonBase-root').should('have.text','Continue').click()
+    cy.get('[id="password"]').type('Welcome2!')
+    cy.get('[type="submit"]').click()
   })
 })
