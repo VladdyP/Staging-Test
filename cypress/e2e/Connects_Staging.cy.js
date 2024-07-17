4ee4f8e6a9497197739edb9d40efe7f2aaf14a77
VladdyP <v.popudrebko@gmail.com>
2024-07-17 14:18:42 -0500

    Added beforEach hook to tests

--- cypress/e2e/Connects_Staging.cy.js
+++ cypress/e2e/Connects_Staging.cy.js
@@ -2,33 +2,35 @@
 
 
 describe('Login Page test', () => {
-  it('Test 01', () => {
+
+  beforeEach("New Tests", () => {
     cy.visit('/cupertino')
     cy.wait(2000)
+  })
+  it('Test 01', () => {
+
     cy.get('[id="userName"]').type('Chris')
     cy.get('.MuiButtonBase-root').should('have.text','Continue').click()
     cy.get('[id="password"]').type('Welcome1!')
     cy.get('[type="submit"]').click()
   }),
+
   it('Test 02', () => {
-    cy.visit('/cupertino')
-    cy.wait(2000)
+
     cy.get('[id="userName"]').type('Chris')
     cy.get('.MuiButtonBase-root').should('have.text','Continue').click()
     cy.get('[id="password"]').type('Welcome1!')
     cy.get('[type="submit"]').click()
   }),
   it('Test 03', () => {
-    cy.visit('/cupertino')
-    cy.wait(2000)
+
     cy.get('[id="userName"]').type('Chris')
     cy.get('.MuiButtonBase-root').should('have.text','Continue').click()
     cy.get('[id="password"]').type('Welcome1!')
     cy.get('[type="submit"]').click()
   }),
   it('Test 04', () => {
-    cy.visit('/cupertino')
-    cy.wait(2000) 
+
     cy.get('[id="userName"]').type('Chris')
     cy.get('.MuiButtonBase-root').should('have.text','Continue').click()
     cy.get('[id="password"]').type('Welcome2!')
