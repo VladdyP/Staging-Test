2c3b9966523e893f59404fd55b1df20e4b6a8b85
VladdyP <v.popudrebko@gmail.com>
2024-08-19 13:27:07 -0500

    Wait time to 2000 sec

--- cypress/e2e/Profile_page.cy.js
+++ cypress/e2e/Profile_page.cy.js
@@ -4,7 +4,7 @@ describe('Profile Page', () => {
 
     beforeEach(() => {
         cy.visit('/cupertino')
-        cy.wait(1000)
+        cy.wait(2000)
         cy.get('[id="userName"]').type('Chris')
         cy.get("button#onetrust-accept-btn-handler").should('be.visible').click()
         cy.get('.MuiButtonBase-root').should('have.text','Continue').click()
