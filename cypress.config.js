20c74b06d7759b1a498d933810b6825f3c4373fd
VladdyP <v.popudrebko@gmail.com>
2026-07-30 11:08:51 -0500

    Add Canyon Cypress project setup

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: '24m16h',
+  env: {
+    canyonBaseUrl: 'https://www.canyon.com/en-us/',
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
